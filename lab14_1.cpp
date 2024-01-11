325cda7f9946fc4ba1a4dcb329d08aee3d046164
Suphaloek Khueanphet <suphaloek_khueanphet@cmu.ac.th>
2024-01-11 23:24:01 +0700

    complete_all

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+    for(int m = 1 ; m < N;m++){
+        for(int j = m ;j > 0 ; j--){
+            if(d[j] > d[j-1]){
+                T temp = d[j];
+                d[j] = d[j-1];
+                d[j-1] = temp;
+            }
+        }
+        cout << "Pass " << m << ":";
+        for(int o = 0; o < N; o++) cout << d[o] << " ";    
+        cout << endl;
 
+    }
 }
 
 int main(){
